bd7237a64119901aa896786ac198077b56e3032a
Abdullah-0110 <abdullahdev88@gmail.com>
2023-12-16 23:52:15 +0500

    types of the array declaration or initilaization

--- initArray.c
+++ initArray.c
@@ -10,4 +10,23 @@ int main(){
 
     int arr1[]={1,2,3,4,5};//valid init compiler is know what is the size of the array
 
+    int arr2[5]={1,2,3};//valid init type b/c the size of the array is greater than the content of the array
+
+    int arr3[5]={0}; //valid init type of the array 
+
+    char var[]={'A','b''d','u','l','l','a','h'};//valid type this array print the char content in the array
+    /*
+    invalid inilization types
+    int var[]={};
+    int var1[2]={1,2,3,4};
+
+    */
+
+   printf("%d\n",arr[0]);//i try to print the index 0 value in the array
+   printf("%d\n",arr1[5]);//i try to print the index 5 value in the array
+   printf("%d\n",arr2[4]);//i try to print the index 4 value in the array
+   printf("%d\n",arr3[3]);//i try to print the index 3 value in the array
+   printf("%c\n",var[0]);//i try to print the index 0 value in the array
+   printf("%d\n");//new line
+
 }
