db12bb27f5944c10cc43cc4f14d4c9514eeb25f6
Abdullah-0110 <abdullahdev88@gmail.com>
2023-12-16 23:43:17 +0500

    Initiliazation types of the array

---
+++ initArray.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    //initiliazation of array 
+    //valide types
+
+    int arr[5]={1,2,3,4,5};//this valide init array type
+
+    //second type
+
+    int arr1[]={1,2,3,4,5};//valid init compiler is know what is the size of the array
+
+}
